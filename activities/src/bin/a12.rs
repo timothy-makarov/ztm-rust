700a74b4f0128f1579380caaf71a2eef8b480c91
Tim Makarov <timothy.makarov@gmail.com>
2023-11-29 21:13:45 +0400

    Activity 12: Implementing Functionality.

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,4 +10,60 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+use std::fmt;
+
+enum BoxColor {
+    Red,
+    Green,
+    Blue,
+}
+
+struct Dimensions {
+    width: i32,
+    height: i32,
+    depth: i32
+}
+
+impl fmt::Debug for BoxColor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            BoxColor::Green => write!(f, "Green"),
+            BoxColor::Red => write!(f, "Red"),
+            BoxColor::Blue => write!(f, "Blue"),
+        }
+    }
+}
+
+struct ShippingBox {
+    dimenstions: Dimensions,
+    weight: i32,
+    color: BoxColor,
+}
+
+impl ShippingBox {
+    fn create(width: i32, height: i32, depth: i32, weight: i32, color: BoxColor) -> Self {
+        ShippingBox {
+            dimenstions: Dimensions { width, height, depth },
+            weight,
+            color,
+        }
+    }
+
+    fn print_info(&self) {
+        println!(
+            "{:?}x{:?}x{:?} {:?} {:?}",
+            self.dimenstions.width, self.dimenstions.height, self.dimenstions.depth, self.weight, self.color
+        )
+    }
+}
+
+fn main() {
+    let red_box = ShippingBox::create(5, 5, 5, 5, BoxColor::Red);
+    red_box.print_info();
+
+    let green_box = ShippingBox::create(50, 50, 50, 50, BoxColor::Green);
+    green_box.print_info();
+
+    let blue_box = ShippingBox::create(500, 500, 500, 500, BoxColor::Blue);
+    blue_box.print_info();
+}
