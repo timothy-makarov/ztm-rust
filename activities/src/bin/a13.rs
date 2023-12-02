38af215a526e612b69d32bb4154bc538ab576990
Tim Makarov <timothy.makarov@gmail.com>
2023-12-02 22:50:29 +0400

    Activity 13: Vectors & For Loops.

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -10,4 +10,15 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    let v = vec![ 10, 20, 30, 40 ];
+
+    for i in v.iter() {
+        match i {
+            30 => println!("thirty"),
+            _ => println!("{:?}", i)
+        }
+    }
+
+    println!("vector length = {:?}", v.len());
+}
