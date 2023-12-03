49d67c300f4e31a8651b24f93ec2f130a3286164
Tim Makarov <timothy.makarov@gmail.com>
2023-12-03 16:28:20 +0400

    Activity 15: Advanced match.

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -11,4 +11,38 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+enum TicketType {
+    Standard,
+    Backstage(String),
+    Vip(String)
+}
+
+struct Ticket {
+    ticket_type: TicketType,
+    price: f32
+}
+
+fn main() {
+    let tickets = vec![
+        Ticket {
+            ticket_type: TicketType::Standard,
+            price: 5.0
+        },
+        Ticket {
+            ticket_type: TicketType::Vip("Sarah".to_owned()),
+            price: 10.0
+        },
+        Ticket {
+            ticket_type: TicketType::Backstage("Jessie".to_owned()),
+            price: 20.0
+        }
+    ];
+
+    for t in tickets {
+        match t.ticket_type {
+            TicketType::Standard => println!("Standard ticket for {:?}", t.price),
+            TicketType::Vip(name) => println!("{:?} VIP ticket for {:?}", name, t.price),
+            TicketType::Backstage(name) => println!("{:?} Backstage ticket for {:?}", name, t.price)
+        }
+    }
+}
