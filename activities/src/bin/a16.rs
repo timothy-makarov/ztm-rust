4ce91803d484cf310b56b1bf2d48d63d6460e0bf
Tim Makarov <timothy.makarov@gmail.com>
2023-12-03 18:09:17 +0400

    Activity 16: Option.

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -8,4 +8,28 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Student {
+    name: String,
+    locker_number: Option<i32>
+}
+
+fn print_student_info(student: Student) {
+    match student.locker_number {
+        Some(n) => println!("{:?} assigned to a locker number {:?}" , student.name, n),
+        None => println!("{:?} has no locker assigned to.", student.name)
+    }
+}
+
+fn main() {
+    let s1 = Student {
+        name: "Peter".to_owned(),
+        locker_number: Some(42)
+    };
+    print_student_info(s1);
+
+    let l2 = Student {
+        name: "Kyle".to_owned(),
+        locker_number: None
+    };
+    print_student_info(l2);
+}
