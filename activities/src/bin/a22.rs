efbb95bea133a26fc7937fa3e1e20b0d878c4be4
Tim Makarov <timothy.makarov@gmail.com>
2024-02-09 01:35:01 +0400

    Activity 22: Testing.

--- activities/src/bin/a22.rs
+++ activities/src/bin/a22.rs
@@ -23,12 +23,96 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
-    Some(a / b)
+    if b == 0 {
+        None
+    } else {
+        Some(a / b)
+    }
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod test_suite {
+    use crate::*;
+
+    #[test]
+    fn test_clamp_1_0_2() {
+        assert_eq!(clamp(1, 0, 2), 1);
+    }
+
+    #[test]
+    fn test_clamp_m1_0_2() {
+        assert_eq!(clamp(-1, 0, 2), 0);
+    }
+
+    #[test]
+    fn test_clamp_300_0_2() {
+        assert_eq!(clamp(300, 0, 2), 2);
+    }
+
+    #[test]
+    fn test_clamp_0_0_2() {
+        assert_eq!(clamp(0, 0, 2), 0);
+    }
+
+    #[test]
+    fn test_clamp_2_0_2() {
+        assert_eq!(clamp(2, 0, 2), 2);
+    }
+
+    #[test]
+    fn test_clamp_m2_m4_m1() {
+        assert_eq!(clamp(-2, -4, -1), -2);
+    }
+
+    #[test]
+    fn test_clamp_m5_m4_m1() {
+        assert_eq!(clamp(-5, -4, -1), -4);
+    }
+
+    #[test]
+    fn test_div_4_2() {
+        assert_eq!(div(4, 2), Some(2));
+    }
+
+    #[test]
+    fn test_div_0_2() {
+        assert_eq!(div(0, 2), Some(0));
+    }
+
+    #[test]
+    fn test_div_4_0() {
+        assert_eq!(div(4, 0), None);
+    }
+
+    #[test]
+    fn test_concat_empty() {
+        assert_eq!(concat("", ""), "");
+    }
+
+    #[test]
+    fn test_concat_foo_bar() {
+        assert_eq!(concat("foo", "bar"), "foobar");
+    }
+
+    #[test]
+    fn test_concat_123_qwerty() {
+        assert_eq!(concat("123", "qwerty"), "123qwerty");
+    }
+
+    #[test]
+    fn test_concat_hello_empty() {
+        assert_eq!(concat("hello", ""), "hello");
+    }
+
+    #[test]
+    fn test_concat_empty_hello() {
+        assert_eq!(concat("", "hello"), "hello");
+    }
+}
