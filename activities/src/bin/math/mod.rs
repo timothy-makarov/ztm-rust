3c2dade6fefc863c732c3cba7d2daac744702cd7
Tim Makarov <timothy.makarov@gmail.com>
2024-02-15 00:18:04 +0400

    Activity 26c: External Modules.

---
+++ activities/src/bin/math/mod.rs
@@ -0,0 +1,13 @@
+pub mod math {
+    pub fn add(lhs: isize, rhs: isize) -> isize {
+        lhs + rhs
+    }
+    
+    pub fn sub(lhs: isize, rhs: isize) -> isize {
+        lhs - rhs
+    }
+    
+    pub fn mul(lhs: isize, rhs: isize) -> isize {
+        lhs * rhs
+    }
+}
